34045f5ba7f1acdba3258bb97b259cdb6352b156
nreddy86 <nreddy86@gmail.com>
2019-10-31 12:31:07 +0530

    authentication changes

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using ARUP.Context;
+using ARUP.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace ARUP.Controllers
+{
+    [RoutePrefix("api/authentication")]
+    public class AuthenticationController : ApiController
+    {
+        DataContext db = new DataContext();
+
+        /// <summary>
+        /// Validate the user
+        /// </summary>
+        /// <returns></returns>
+        // POST: api/authentication/validateuser
+        [HttpPost]
+        [Route("validateuser")]
+        public IHttpActionResult ValidateUser([FromBody]Users user)
+        {
+            var result = (from usr in db.Users where usr.Username == user.Username && usr.Password == user.Password select usr).FirstOrDefault();
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Validate the user
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("getuser")]
+        public IHttpActionResult GetUser()
+        {
+            return Ok("success");
+        }
+    }
+}
